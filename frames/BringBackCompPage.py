cb4199431f8afc93e1fa110ed1b13c46408fd36b
Szymon Kozyra <szymon6678@gmail.com>
2023-01-25 10:39:54 +0100

    testing

--- frames/BringBackCompPage.py
+++ frames/BringBackCompPage.py
@@ -2,16 +2,10 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from customtkinter import *
-<<<<<<< HEAD:frames/BringBackCompPage.py
 from functions.sql_commands import *
 from functions.styles import *
 from functions.connection import *
 from functions.suggestions import *
-=======
-from styles import *
-import connection
-import suggestions
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringBackCompPage.py
 
 
 class BringBackCompPage(CTkFrame):
@@ -20,19 +14,10 @@ class BringBackCompPage(CTkFrame):
         from frames.StartPage import StartPage
         CTkFrame.__init__(self, master)
         style_bb_comp_page(master)
-<<<<<<< HEAD:frames/BringBackCompPage.py
         create_conn = connection()
 
         def update_revision_list(*args):
             self.revision_py["values"] = list_revision(self)
-=======
-        conn = connection.connection()
-        session = connection.session()
-
-
-        def update_revision_list(*args):
-            self.revision_py["values"] = suggestions.list_revision(self)
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringBackCompPage.py
 
         def button_action():
             if select_sql() is not None:
@@ -77,25 +62,14 @@ class BringBackCompPage(CTkFrame):
             else:
                 master.switch_frame(StartPage)
 
-<<<<<<< HEAD:frames/BringBackCompPage.py
         self.partnumber_py = ttk.Combobox(self, width=30, values=list_partnumber())
         self.partnumber_py.grid(row=0, column=1)
         self.partnumber_py.bind('<KeyRelease>', lambda event: search(self, event))
-=======
-
-        self.partnumber_py = ttk.Combobox(self, width=30, values=suggestions.list_partnumber())
-        self.partnumber_py.grid(row=0, column=1)
-        self.partnumber_py.bind('<KeyRelease>', lambda event: suggestions.search(self, event))
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringBackCompPage.py
         # self.partnumber_py.bind('<KeyRelease>', update_revision_list)
         self.partnumber_py.bind("<FocusOut>", update_revision_list)
         self.partnumber_py.bind("<<ComboboxSelected>>", update_revision_list)
         self.partnumber_py.bind("<Return>", update_revision_list)
-<<<<<<< HEAD:frames/BringBackCompPage.py
         self.revision_py = ttk.Combobox(self, width=30, values=list_revision(self))
-=======
-        self.revision_py = ttk.Combobox(self, width=30, values=suggestions.list_revision(self))
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringBackCompPage.py
         self.revision_py.grid(row=1, column=1)
         self.quantity_py = CTkEntry(self, width=200)
         self.quantity_py.grid(row=4, column=1)
--- frames/BringCompPage.py
+++ frames/BringCompPage.py
@@ -2,16 +2,10 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from customtkinter import *
-<<<<<<< HEAD:frames/BringCompPage.py
 from functions.sql_commands import *
 from functions.styles import *
 from functions.connection import *
 from functions.suggestions import *
-=======
-from styles import *
-import connection
-import suggestions
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringCompPage.py
 
 
 class BringCompPage(CTkFrame):
@@ -20,18 +14,10 @@ class BringCompPage(CTkFrame):
         from frames.StartPage import StartPage
         CTkFrame.__init__(self, master)
         style_bring_comp_page(master)
-<<<<<<< HEAD:frames/BringCompPage.py
         create_conn = connection()
 
         def update_revision_list(*args):
             self.revision_py["values"] = list_revision(self)
-=======
-        conn = connection.connection()
-        session = connection.session()
-
-        def update_revision_list(*args):
-            self.revision_py["values"] = suggestions.list_revision(self)
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringCompPage.py
 
         def button_action():
             if select_sql() is not None:
@@ -76,24 +62,14 @@ class BringCompPage(CTkFrame):
             else:
                 master.switch_frame(StartPage)
 
-<<<<<<< HEAD:frames/BringCompPage.py
         self.partnumber_py = ttk.Combobox(self, width=30, values=list_partnumber())
         self.partnumber_py.grid(row=0, column=1)
         self.partnumber_py.bind('<KeyRelease>', lambda event: search(self, event))
-=======
-        self.partnumber_py = ttk.Combobox(self, width=30, values=suggestions.list_partnumber())
-        self.partnumber_py.grid(row=0, column=1)
-        self.partnumber_py.bind('<KeyRelease>', lambda event: suggestions.search(self, event))
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringCompPage.py
         # self.partnumber_py.bind('<KeyRelease>', update_revision_list)
         self.partnumber_py.bind("<FocusOut>", update_revision_list)
         self.partnumber_py.bind("<<ComboboxSelected>>", update_revision_list)
         self.partnumber_py.bind("<Return>", update_revision_list)
-<<<<<<< HEAD:frames/BringCompPage.py
         self.revision_py = ttk.Combobox(self, width=30, values=list_revision(self))
-=======
-        self.revision_py = ttk.Combobox(self, width=30, values=suggestions.list_revision(self))
->>>>>>> 0d7faac51f4648ea3a88ec37507c14b08e8fa3a9:BringCompPage.py
         self.revision_py.grid(row=1, column=1)
         self.quantity_py = CTkEntry(self, width=200)
         self.quantity_py.grid(row=4, column=1)
